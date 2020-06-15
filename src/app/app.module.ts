58de25e3199ebe0ae58d4f7abbcbb0661d62071e
nit159 <arun.agarwal@neptunr-ubi.com>
2020-06-15 13:28:25 +0530

    [ON-34] feat:add new list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReimburstmentViewComponent } from './reimburstment-view/reimburstment-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReimburstmentViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/reimburstment-view/reimburstment-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reimburstment-view',
+  templateUrl: './reimburstment-view.component.html',
+  styleUrls: ['./reimburstment-view.component.css']
+})
+export class ReimburstmentViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
